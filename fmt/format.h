3cefa72f2019616cc3610df9554743f74e4ade92
Dmitry Panteleev <dpantele@example.com>
2016-05-16 20:07:14 -0400

    Workaround for missing lconv support in android (#327)

--- fmt/format.h
+++ fmt/format.h
@@ -910,6 +910,14 @@ class ThousandsSep {
   }
 };
 
+// Returns the thousands separator for the current locale.
+template <bool SUPPORTED>
+fmt::StringRef thousands_sep() { return std::localeconv()->thousands_sep; }
+
+// Android doesn't have lconv::thousands_sep.
+template <>
+inline fmt::StringRef thousands_sep<false>() { return ""; }
+
 // Formats a decimal unsigned integer value writing into buffer.
 // thousands_sep is a functor that is called after writing each char to
 // add a thousands separator if necessary.
@@ -2778,7 +2786,8 @@ void BasicWriter<Char>::write_int(T value, Spec spec) {
   }
   case 'n': {
     unsigned num_digits = internal::count_digits(abs_value);
-    fmt::StringRef sep = std::localeconv()->thousands_sep;
+    fmt::StringRef sep =
+        internal::thousands_sep<(sizeof(lconv) >= sizeof(char*))>();
     unsigned size = static_cast<unsigned>(
           num_digits + sep.size() * (num_digits - 1) / 3);
     CharPtr p = prepare_int_buffer(size, spec, prefix, prefix_size) + 1;
